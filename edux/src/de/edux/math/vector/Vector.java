51b44cd73ba9d21417092b5b5229402420f9a591
beep112 <88689885+beep112@users.noreply.github.com>
2023-09-27 01:41:49 -0400

    Added constructors for vector class

--- edux/src/de/edux/math/vector/Vector.java
+++ edux/src/de/edux/math/vector/Vector.java
@@ -1,5 +1,19 @@
 package de.edux.math.vector;
 
 public class Vector {
-
+	// implemenation for column vectors
+	private int column;
+	private double[] data;
+	
+	public Vector(int column) {
+		this.column = column;
+		data = new double[column];
+	}
+	
+	public Vector(double[] data) {
+		column = data.length;
+		this.data = data;
+	}
+	
+	
 }
